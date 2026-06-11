6cf60973f7bda0a1b9f716369c0962630c03f3c3
rajeshpal33 <2007palrajesh@gmail.com>
2026-06-11 19:14:16 +0530

    Added Menu Driven student management system

--- student_management.c
+++ student_management.c
@@ -1,36 +1,69 @@
-#include<stdio.h>
+#include <stdio.h>
 
-struct student{
-	int roll;
-	char name[50];
-	float marks;
+struct Student
+{
+    int roll;
+    char name[50];
+    float marks;
+};
+
+int main()
+{
+    struct Student s[100];
+    int totalStudents = 0;
+    int choice, i;
+
+    while(1)
+    {
+        printf("\n===== STUDENT MANAGEMENT SYSTEM =====\n");
+        printf("1. Add Student\n");
+        printf("2. View Students\n");
+        printf("3. Exit\n");
+
+        printf("Enter Choice: ");
+        scanf("%d", &choice);
+
+        switch(choice)
+        {
+            case 1:
+                printf("\nEnter Roll Number: ");
+                scanf("%d", &s[totalStudents].roll);
+
+                printf("Enter Name: ");
+                scanf("%s", s[totalStudents].name);
+
+                printf("Enter Marks: ");
+                scanf("%f", &s[totalStudents].marks);
+
+                totalStudents++;
+
+                printf("Student Added Successfully!\n");
+                break;
+
+            case 2:
+                if(totalStudents == 0)
+                {
+                    printf("\nNo Students Found!\n");
+                }
+                else
+                {
+                    printf("\n===== STUDENT RECORDS =====\n");
+
+                    for(i = 0; i < totalStudents; i++)
+                    {
+                        printf("\nRoll No : %d\n", s[i].roll);
+                        printf("Name    : %s\n", s[i].name);
+                        printf("Marks   : %.2f\n", s[i].marks);
+                    }
+                }
+                break;
+
+            case 3:
+                printf("\nExiting Program...\n");
+                return 0;
+
+            default:
+                printf("\nInvalid Choice!\n");
+        }
+    }
 }
-
-int main(){
-	int i,n;
-	printf("How many students: ");
-	scanf("%d", &n);
-
-	struct student s[n];
-	for(int i=0;i<n;i++){
-		printf("Enter Roll no.: ");
-		scanf("%d", &s[i].roll);
-
-		printf("Enter Name: ");
-		scanf("%s", s[i].name);
-
-		printf("Enter Marks: ");
-		scanf("%f", &marks);
-}
-
-
-	printf("\n--------STUDENT DETAILS--------");
-	for(int i=0;i<n;i++){
-		printf("Student %d\n", i+1);
-		printf("Roll No.: %d\n", s[i].roll);
-		printf("Name: %s\n", s[i].name);
-		printf("Marks: %f\n", s[i].marks);
-}
-return 0;
-}
-
